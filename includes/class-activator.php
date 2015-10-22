bf2ff6ba46cda32be8305023822b45020b805b63
EdwardBock <EdwardBock@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-10-22 11:49:12 +0000

    update
    git-svn-id: https://plugins.svn.wordpress.org/postqueue/tags/1.0.6@1271037 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/class-activator.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * Fired during plugin activation.
+ *
+ */
+class Ph_Postqueue_Activator {
+
+	/**
+	 * activate plugin
+	 */
+	public static function activate() {
+		/**
+		 * wpdb object for prefix
+		 */
+		global $wpdb;
+		/**
+		 * require upgrade.php for dbDelta function
+		 */
+		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+		/**
+		 * Create queues table
+		 */
+		dbDelta('CREATE TABLE IF NOT EXISTS `'.$wpdb->prefix."ph_postqueues` (
+				  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+				  `name` varchar(30) NOT NULL DEFAULT '',
+				  `slug` varchar(30) NOT NULL DEFAULT '',
+				  PRIMARY KEY (`id`),
+				  UNIQUE KEY `slug` (`slug`),
+				  KEY `name` (`name`)
+				) ENGINE=MyISAM DEFAULT CHARSET=utf8;");
+
+		/**
+		 * create queues_content table
+		 */
+		dbDelta( 'CREATE TABLE IF NOT EXISTS `'.$wpdb->prefix."ph_postqueue_contents` (
+				  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+				  `queue_id` int(11) unsigned NOT NULL,
+				  `post_id` int(11) unsigned NOT NULL,
+				  `position` int(11) unsigned NOT NULL DEFAULT 0,
+				  `title_overwrite` varchar(255) NOT NULL DEFAULT '',
+				  PRIMARY KEY (`id`),
+				  UNIQUE KEY `post_in_queue` (`post_id`, `queue_id`),
+				  UNIQUE KEY `position_in_queue` (`position`, `queue_id`),
+				  KEY `queue_id` (`queue_id`)
+				) ENGINE=MyISAM DEFAULT CHARSET=utf8;");
+	}
+
+}
---
+++ includes/class-ph-postqueue-store.php
@@ -0,0 +1,214 @@
+<?php
+/**
+ *	Postqueue Store class
+ */
+class PH_Postqueue_Store
+{
+
+	/**
+	 * postqueues
+	 */
+	private $queues;
+
+	/**
+	 * Define the core functionality of the plugin.
+	 */
+	public function __construct() {
+		$this->queues = null;
+	}
+
+	/**
+	 * creates a new queue
+	 */
+	public function create($name)
+	{
+		global $wpdb;
+		$result = (object) array();
+		$result->name = $name;
+		$result->slug = sanitize_title($result->name);
+		$result->success = $wpdb->insert(
+			$wpdb->prefix.'ph_postqueues',
+			array(
+				'name' => $result->name,
+				'slug' => $result->slug,
+			),
+			array(
+				'%s',
+				'%s',
+			)
+		);
+		$result->id = $wpdb->insert_id;
+		return $result;
+
+	}
+
+	/**
+	 * Adds a single relation
+	 * @return  queues array
+	 */
+	public function get_queues()
+	{
+		if($this->queues == null)
+		{
+			$this->queues = $this->search();
+		}
+		return $this->queues;
+	}
+
+	/**
+	 * returns queue by id
+	 */
+	public function get_queue_by_id($qid)
+	{
+		return $this->get_queue('queue_id',$qid);
+	}
+
+	/**
+	 * returns queue by slug
+	 */
+	public function get_queue_by_slug($slug)
+	{
+		return $this->get_queue('slug',$slug);
+
+	}
+	private function get_queue($key, $value){
+
+		global $wpdb;
+		$query = "";
+		$query.= "SELECT name, slug, contents.id as cid, queue_id, post_id, position, title_overwrite as title FROM";
+		$query.=" ".$wpdb->prefix."ph_postqueues queue LEFT JOIN ".$wpdb->prefix."ph_postqueue_contents contents";
+		$query.= " ON (queue.id = contents.queue_id)";
+		$query.=" WHERE $key = '$value'";
+		$query.=" ORDER BY position ASC";
+
+		$results = $wpdb->get_results($query);
+		for($i = 0; $i < count($results); $i++) {
+			if ( FALSE === get_post_status( $results[$i]->post_id ) ) {
+				unset($results[$i]);
+				continue;
+			}
+			$pid = $results[$i]->post_id;
+			if($results[$i]->title != ""){
+				$results[$i]->post_title = $results[$i]->title;
+			} else{
+				$results[$i]->post_title = get_the_title($pid);
+			}
+		}
+		return $results;
+	}
+
+	public function queue_clear($queue_id)
+	{
+		global $wpdb;
+		$wpdb->delete(
+			$wpdb->prefix."ph_postqueue_contents",
+			array( "queue_id" => $queue_id ),
+			array( "%d" )
+		);
+	}
+
+	public function queue_add_all($qid, $post_ids)
+	{
+		foreach ($post_ids as $position => $post_id) {
+			$this->queue_add($qid, $post_id, $position);
+		}
+	}
+
+	public function queue_add_all_with_title($qid, $post_ids, $titles)
+	{
+
+		for ($i=0; $i < count($post_ids) ; $i++) {
+			$this->queue_add($qid, $post_ids[$i], $i, $titles[$i]);
+		}
+	}
+
+	public function queue_add($queue_id, $post_id, $position, $title = "")
+	{
+		global $wpdb;
+		$wpdb->insert(
+			$wpdb->prefix."ph_postqueue_contents",
+			array(
+				'queue_id' => $queue_id,
+				'post_id' => $post_id,
+				'position' => $position,
+				'title_overwrite' => $title,
+			),
+			array(
+				"%d",
+				"%d",
+				"%d",
+				'%s',
+			)
+		);
+	}
+
+	public function delete_queue($queue_id)
+	{
+		global $wpdb;
+		$wpdb->delete(
+			$wpdb->prefix."ph_postqueue_contents",
+			array(
+				"queue_id" => $queue_id,
+			),
+			array(
+				"%d",
+			)
+		);
+		$wpdb->delete(
+			$wpdb->prefix."ph_postqueues",
+			array(
+				"id" => $queue_id,
+			),
+			array(
+				"%d",
+			)
+		);
+	}
+
+	public function delete_queue_post($queue_id, $post_id)
+	{
+		global $wpdb;
+		$wpdb->delete(
+			$wpdb->prefix."ph_postqueue_contents",
+			array(
+				"post_id" => $post_id,
+				"queue_id" => $queue_id,
+			),
+			array(
+				"%d",
+				"%d",
+			)
+		);
+	}
+
+	/**
+	 * delets all queue contents of the deleted post id
+	 */
+	public function clear_for_post_id($post_id)
+	{
+		global $wpdb;
+		$wpdb->delete(
+			$wpdb->prefix."ph_postqueue_contents",
+			array(
+				"post_id" => $post_id,
+			),
+			array(
+				"%d",
+			)
+		);
+	}
+
+	/**
+	 * serach queue
+	 */
+	public function search($name = ""){
+		global $wpdb;
+		$query = "";
+		$query.= "SELECT * FROM ".$wpdb->prefix."ph_postqueues";
+		$query.=" WHERE name LIKE '%".$name."%'";
+		$query.=" ORDER BY id ASC";
+		$result = $wpdb->get_results($query);
+		return $result;
+	}
+}
+?>
